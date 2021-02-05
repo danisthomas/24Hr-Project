e33ce2fb4267ab4aa168ec757a68c128fc070026
danisthomas <danielesthomas6@gmail.com>
2021-02-04 19:56:36 -0500

    config setup

---
+++ 24Hr.Data/Comment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hr.Data
+{
+   public class Comment
+   {
+        [Key]
+        public int CommentId { get; set; }
+        [Required]
+        public string CommentText { get; set; }
+        [Required]
+        public Guid CommentAuthor { get; set; }
+
+        [ForeignKey(nameof(Replies))]
+        public int ReplyId { get; set; }
+
+        public virtual Reply Replies { get; set; }
+
+        public DateTimeOffset CommentCreatedUtc { get; set; }
+
+        public Guid Author { get; set; }
+
+
+    }
+}
---
+++ 24Hr.Data/Reply.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hr.Data
+{
+    public class Reply
+    {
+        [Key]
+        [Required]
+        public int ReplyId { get; set; }
+
+        [Required]
+        public string ReplyText { get; set; }
+
+        public DateTimeOffset ReplyCreatedUtc { get; set; }
+
+        [Required]
+        public Guid Author { get; set; }
+
+
+    }
+}
